d97f90654a20d484174ec3135367018526400d35
LucasHSantana <lukaz.santana@gmail.com>
2019-02-25 20:30:55 -0300

    Criada estrutura do arquivo principal

---
+++ busca_cep.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait, Select 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import os
+import time
+
+class CEP:
+    def __init__(self):
+        #Inicializa o chromedriver
+        pass
+
+    def __del__(self):
+        #Finaliza o chromedriver
+        pass
+
+    def get_cep_range(self, uf):
+        #Cria uma lista com as faixas de ceps pelo estado
+        pass
+
+    def get_endereco_unico(cep):
+        #Consulta um cep e cria um dicionário com as informações encontradas
+        pass
+
+    def get_enderecos(self, faixa):
+        #Consulta todos os ceps na faixa indicada
+        pass
+
+# Se for o arquivo principal...
+if __name__ == '__main__':
+    def start:
+        #Inicia o orquestrador
+
+start()
+    
